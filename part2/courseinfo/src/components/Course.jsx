a5e84b8be910d5c74720b4d099d77277cd9a250e
Bijo Joseph <bijo.joseph22@gmail.com>
2024-02-27 08:57:31 -0800

    Fix formatting.

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -8,38 +8,38 @@ const Course = (props) => {
     )
   }
   
-  const Header = (props) => {
-    return (
-      <h1>{props.header}</h1>
-    )
-  }
-  
-  const Content = (props) => {
-    return (
-      <div>
-        {props.content.map(part => <Part key={part.id} part={part}/>)}
-      </div>
-    )
-  }
-  
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    )
-  }
-  
-  const Total = (props) => {
-    const numberOfExercises = props.parts.reduce((exercises, part) => {
-      exercises += part.exercises
-      return exercises
-    },0);
-  
-  
-    return (
-      <b>Total of {numberOfExercises} exercises</b>
-    )
-  }
+const Header = (props) => {
+  return (
+    <h1>{props.header}</h1>
+  )
+}
 
-  export default Course
+const Content = (props) => {
+  return (
+    <div>
+      {props.content.map(part => <Part key={part.id} part={part}/>)}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  const numberOfExercises = props.parts.reduce((exercises, part) => {
+    exercises += part.exercises
+    return exercises
+  },0);
+
+
+  return (
+    <b>Total of {numberOfExercises} exercises</b>
+  )
+}
+
+export default Course
